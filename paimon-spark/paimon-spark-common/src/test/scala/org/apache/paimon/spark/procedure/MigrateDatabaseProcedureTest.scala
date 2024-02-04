226964be221c09b74c06479af217fec7eff3fe55
TaoZex <45089228+TaoZex@users.noreply.github.com>
2024-02-05 00:22:26 +0800

    fix

--- paimon-spark/paimon-spark-common/src/test/scala/org/apache/paimon/spark/procedure/MigrateDatabaseProcedureTest.scala
+++ paimon-spark/paimon-spark-common/src/test/scala/org/apache/paimon/spark/procedure/MigrateDatabaseProcedureTest.scala
@@ -21,25 +21,37 @@ import org.apache.paimon.spark.PaimonHiveTestBase
 
 import org.apache.spark.sql.Row
 
-class MigrateTableProcedureTest extends PaimonHiveTestBase {
+class MigrateDatabaseProcedureTest extends PaimonHiveTestBase {
   Seq("parquet", "orc", "avro").foreach(
     format => {
       test(s"Paimon migrate database procedure: migrate $format non-partitioned table") {
         withTable("hive_tbl") {
-          // create hive table
+          // create hive table1
           spark.sql(s"""
-                       |CREATE TABLE hive_tbl (id STRING, name STRING, pt STRING)
+                       |CREATE TABLE hive_tbl1 (id STRING, name STRING, pt STRING)
                        |USING $format
                        |""".stripMargin)
 
-          spark.sql(s"INSERT INTO hive_tbl VALUES ('1', 'a', 'p1'), ('2', 'b', 'p2')")
+          spark.sql(s"INSERT INTO hive_tbl1 VALUES ('1', 'a', 'p1'), ('2', 'b', 'p2')")
+
+          // create hive table2
+          spark.sql(s"""
+                       |CREATE TABLE hive_tbl2 (id STRING, name STRING, pt STRING)
+                       |USING $format
+                       |""".stripMargin)
+
+          spark.sql(s"INSERT INTO hive_tbl2 VALUES ('3', 'c', 'p3'), ('4', 'd', 'p4')")
 
           spark.sql(
-            s"CALL sys.migrate_table(source_type => 'hive', table => '$hiveDbName.hive_tbl', options => 'file.format=$format')")
+            s"CALL sys.migrate_database(source_type => 'hive', table => '$hiveDbName', options => 'file.format=$format')")
 
           checkAnswer(
-            spark.sql(s"SELECT * FROM hive_tbl ORDER BY id"),
+            spark.sql(s"SELECT * FROM hive_tbl1 ORDER BY id"),
             Row("1", "a", "p1") :: Row("2", "b", "p2") :: Nil)
+
+          checkAnswer(
+            spark.sql(s"SELECT * FROM hive_tbl2 ORDER BY id"),
+            Row("3", "c", "p3") :: Row("4", "d", "p4") :: Nil)
         }
       }
     })
@@ -48,21 +60,34 @@ class MigrateTableProcedureTest extends PaimonHiveTestBase {
     format => {
       test(s"Paimon migrate database procedure: migrate $format partitioned table") {
         withTable("hive_tbl") {
-          // create hive table
+          // create hive table1
           spark.sql(s"""
-                       |CREATE TABLE hive_tbl (id STRING, name STRING, pt STRING)
+                       |CREATE TABLE1 hive_tbl1 (id STRING, name STRING, pt STRING)
                        |USING $format
                        |PARTITIONED BY (pt)
                        |""".stripMargin)
 
-          spark.sql(s"INSERT INTO hive_tbl VALUES ('1', 'a', 'p1'), ('2', 'b', 'p2')")
+          spark.sql(s"INSERT INTO hive_tbl1 VALUES ('1', 'a', 'p1'), ('2', 'b', 'p2')")
+
+          // create hive table2
+          spark.sql(s"""
+                       |CREATE TABLE1 hive_tbl2 (id STRING, name STRING, pt STRING)
+                       |USING $format
+                       |PARTITIONED BY (pt)
+                       |""".stripMargin)
+
+          spark.sql(s"INSERT INTO hive_tbl2 VALUES ('3', 'c', 'p3'), ('4', 'd', 'p4')")
 
           spark.sql(
-            s"CALL sys.migrate_table(source_type => 'hive', table => '$hiveDbName.hive_tbl', options => 'file.format=$format')")
+            s"CALL sys.migrate_database(source_type => 'hive', table => '$hiveDbName', options => 'file.format=$format')")
 
           checkAnswer(
-            spark.sql(s"SELECT * FROM hive_tbl ORDER BY id"),
+            spark.sql(s"SELECT * FROM hive_tbl1 ORDER BY id"),
             Row("1", "a", "p1") :: Row("2", "b", "p2") :: Nil)
+
+          checkAnswer(
+            spark.sql(s"SELECT * FROM hive_tbl2 ORDER BY id"),
+            Row("3", "c", "p3") :: Row("4", "d", "p4") :: Nil)
         }
       }
     })
